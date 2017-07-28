48cd55009d1d80ee7279c0dcdeee3e1618e209d3
dimka388 <dimka388@gmail.com>
2017-07-28 18:06:14 +0300

    update

--- src/cart-component/index.jsx
+++ src/cart-component/index.jsx
@@ -1,7 +1,21 @@
 'use strict';
 
 import React, { Component } from 'react';
+import 'isomorphic-fetch';
 import './cart.scss';
+
+fetch('https://597ae01bc57781001187654c.mockapi.io/items')
+	.then(function(response) {
+		if (response.status >= 400) {
+			throw new Error("Bad response from server");
+		}
+		return response.json();
+	})
+	.then(function(items) {
+		items.map((item) => {
+			console.log(item)
+		})
+	});
 
 class CartComponent extends Component {
 	render() {
@@ -9,44 +23,6 @@ class CartComponent extends Component {
 			<div className='cart-section'>
 				<div className='holder'>
 					<ul className='list'>
-						<li>
-							<div className='image'>
-								image
-							</div>
-							<div className='text'>
-								<h2>Title Lorem ipsum dolor sit</h2>
-								<p>Lorem ipsum dolor sit amet, quis dictum mauris erat aliquam, ac in pede pharetra quis non et.</p>
-								<select>
-									<option value='SKU'>SKU</option>
-								</select>
-							</div>
-							<div className='price'>
-								<button>remove</button>
-								<button>-</button>
-								<span>5</span>
-								<button>+</button>
-								<strong>75.00 €</strong>
-							</div>
-						</li>
-						<li>
-							<div className='image'>
-								image
-							</div>
-							<div className='text'>
-								<h2>Title Lorem ipsum dolor sit</h2>
-								<p>Lorem ipsum dolor sit amet, quis dictum mauris erat aliquam, ac in pede pharetra quis non et.</p>
-								<select>
-									<option value='SKU'>SKU</option>
-								</select>
-							</div>
-							<div className='price'>
-								<button>remove</button>
-								<button>-</button>
-								<span>5</span>
-								<button>+</button>
-								<strong>75.00 €</strong>
-							</div>
-						</li>
 						<li>
 							<div className='image'>
 								image
